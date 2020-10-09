e4331887e5e75cc03deae069e18d69e907b9d4ce
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-10-08 18:30:19 -0700

    first pass -> moving_zeroes.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,8 +2,8 @@
 Input: an integer
 Returns: an integer
 '''
-    #5 - Module 3: A First-Pass Solution
-    # Your code here	
+#5 - Module 3: A First-Pass Solution
+# Your code here	
 # Empty the Most Jars Algo: the monster reduces the number of distinct jars by as many as he can for each move
 # Binary Algo: the monster takes 2^k cookies from all jars that contain at least 2^k cookies for k as large as possible
 
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,12 +2,37 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+#2 - Module 3: A First-Pass Solution
+# traverse the given array 'arr' from left to right
+# while traversing, maintain count of non-zero elements in array
+# let the count be 'count' 
+# for every non-zero arr[i], put the element at 'arr[count]' and increment 'count'. 
+# after complete traversal, all non-zero elements have already been shifted to front end and 'count' is set as index of first 0
+# now all we need to do is that run a loop which makes all elements zero from 'count' till end of the array
+
+
+# function which pushes all zeros to the end of an array
 def moving_zeroes(arr):
-    # Your code here
+    # count stores index of next avaialable position
+    count = 0 # Count for non-zero elements
+    mock_arr = arr.copy()
 
-    pass
+    if len(arr) == 0:
+        return arr
 
+    # traverse the array - access each and every element of the array for a specific purpose like counting
+    for i in mock_arr:
+        # if current element is zero,
+        if i == 0:
+            # then replace the element at index 'count' with this element
+            # here count is incremented
+            arr.remove(i)
+            arr.append(i)
+            count += 1
 
+    return arr
+
+# move all zeros present in the list to the end
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
